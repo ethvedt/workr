31771880e5ca97ac210e122055064598916a08d1
Eric Thvedt <ericsthvedt@gmail.com>
2023-05-05 13:02:08 -0700

    kanbanboard - WIP

--- client/src/components/KanbanBoard.js
+++ client/src/components/KanbanBoard.js
@@ -4,36 +4,20 @@ import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { userProjectsAtom, userTodosAtom } from '../recoil/state';
 
-const todoStatus = {
-    notStarted: {
-        name: 'Not Started',
-        items: []
-    },
-    inProgress: {
-        name: 'In Progress',
-        items: []
-    },
-    testing: {
-        name: 'Testing',
-        items: []
-    },
-    complete: {
-        name: 'Complete',
-        items: []
-    },
-};
+
 
 function handleDragEnd(result, columns, setColumns) {
     if (!result.destination) return;
     const {source, destination} = result;
-
     if (source.droppableId !== destination.droppableId) {
         const sourceColumn = columns[source.droppableId];
         const destColumn = columns[destination.droppableId];
         const sourceItems = [...sourceColumn.items];
         const destItems = [...destColumn.items];
         const [removed] = sourceItems.splice(source.index, 1);
-        destItems.splice(destination.index, 0, removed);
+        const toReplace = {...removed};
+        toReplace.status = destColumn.name.toLowerCase();
+        destItems.splice(destination.index, 0, toReplace);
         setColumns({
             ...columns,
             [source.droppableId]: {
@@ -62,39 +46,51 @@ function handleDragEnd(result, columns, setColumns) {
 
 export default function KanbanBoard({ project }) {
 
-    const projects = useRecoilValue(userProjectsAtom);
     const [userTodos, setUserTodos] = useRecoilState(userTodosAtom);
-    const [columns, setColumns] = useState(todoStatus);
+    const [columns, setColumns] = useState({});
 
     const todoProject = project.todos;
-
+    
     useEffect(() => {
+        const todoStatus = {
+            notStarted: {
+                name: 'Not Started',
+                items: []
+            },
+            inProgress: {
+                name: 'In Progress',
+                items: []
+            },
+            testing: {
+                name: 'Testing',
+                items: []
+            },
+            complete: {
+                name: 'Complete',
+                items: []
+            },
+        };
+
         for (const td of todoProject) {
-            for (const prop in columns) {
-                const column = columns[prop];
-                const todos = column.items
-                console.log(td, todos, column);
+            for (const prop in todoStatus) {
+                const column = todoStatus[prop];
+                const todos = [...column.items];
                 if(!(td in todos) && (td.status == column.name.toLowerCase())) {
-                    setColumns({
-                        ...columns,
-                        [prop]: {
-                            ...column,
-                            items: [...todos, td]
-                        }
-        
-                    })
+                    todoStatus[prop].items.push(td);
                 }
             }
             
-        }
+        };
 
-    }, [projects]);
+        setColumns(todoStatus);
+
+    }, [userTodos]);
 
     function handleSave(e) {
         for (const prop in columns) {
             for (const td in prop.items) {
                 console.log(td);
-                fetch('/todos/'+td.id, {
+                fetch(`/todos/${td.id}`, {
                     method: 'PATCH',
                     headers: 'application/json',
                     body: JSON.stringify(td)
@@ -110,9 +106,6 @@ export default function KanbanBoard({ project }) {
                 }
             }
         }
-    
-
-    console.log(columns)
 
     return (
     <div className='kanban-board'>
@@ -152,7 +145,9 @@ export default function KanbanBoard({ project }) {
                                                                 {...provided.draggableProps}
                                                                 {...provided.dragHandleProps}
                                                                 >
-                                                                    {item?.title}
+                                                                    <p>{item?.title}</p>
+                                                                    <p>{item?.status}</p>
+                                                                    <p>{item?.due_date}</p>
                                                                 </div>
                                                             )
                                                         }}
@@ -167,8 +162,8 @@ export default function KanbanBoard({ project }) {
                     )
                 })}
             </div>
-        </DragDropContext>
         <button type='button' onClick={handleSave}>Save Changes</button>
+        </DragDropContext>
     </div>
     )
 };
